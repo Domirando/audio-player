a6b7ba78aa6dc2744c088c46ae9bb3b344207353
Domirando <maisiedev@gmail.com>
2022-01-17 18:19:26 +0500

    changed

---
+++ scripts/musicList.js
@@ -0,0 +1,20 @@
+let allSongs = [
+    {
+        name: 'Heathens',
+        artist: 'Aurora',
+        image: 'heathens',
+        src: 'heathens'
+    },
+    {
+        name: 'I don`t think I love you anymore',
+        artist: 'Alaina Castillo',
+        image: 'anymore',
+        src: 'anymore'
+    },
+    {
+        name: 'First Place',
+        artist: 'bülow',
+        image: 'first place',
+        src: 'first place'
+    }
+]
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,27 @@
-const audio = document.querySelector('audio');
-function playAudio() {
+const container = document.querySelector('.container')
+musicImage = container.querySelector('.card img')
+musicName = container.querySelector('.card .about_song .song_details .song-about .song-name')
+musicArtist = container.querySelector('.card .about_song .song_details .song-about .song-artist')
+audioPlayer  = document.querySelector('.card .song_timer')
+let musicPaused = true;
+
+let musicIndex = 2;
+
+const audio = new Audio();
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex)
+})
+
+function loadMusic(indexNumb) {
+    musicName.innerHTML = allSongs[indexNumb - 1].name;
+    musicArtist.innerHTML = allSongs[indexNumb - 1].artist;
+    musicImage.src = `images/${allSongs[indexNumb - 1].image}.jpg`;
+    audio.src = `../assets/audio/${allSongs[indexNumb - 1]}.mp3`;
+    console.log(indexNumb)
     audio.currentTime = 0;
-    audio.play();
 }
-function pauseAudio() {
-    audio.pause();
+function playAudio(){
+    audio.play();
+
 }
