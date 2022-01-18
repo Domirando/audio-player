40125b3006e75ef16f2098d4ec1c0d00b97ecbd1
Domirando <maisiedev@gmail.com>
2022-01-18 11:03:14 +0500

    fixed

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ let index_no = 0;
 let Playing_song = false;
 
 let track = document.createElement('audio');
-
+let trackDurationMin, trackDurationSec;
 function load_track(index_no){
     // clearInterval(timer);
     reset_slider();
@@ -53,7 +53,7 @@ function playsong(){
     track.play();
     Playing_song = true;
     play.src = "../assets/img/pause.png";
-    change_duration()
+    // change_duration()
 }
 
 function pausesong(){
@@ -75,11 +75,6 @@ function next_song(){
         playsong();
 
     }
-}
-
-function change_duration(){
-    slider_position = track.duration * (slider.value / 100);
-    track.currentTime = slider_position;
 }
 
 function previous_song(){
@@ -106,4 +101,37 @@ function range_slider(){
         play.src = "../assets/img/play.png";
     }
 }
+track.addEventListener("loadeddata", () => {
+    dataChange();
+})
+function dataChange() {
+    let musicCurrentTime = container.querySelector(".currentTime");
+    let audioDuration = track.duration;
+    let totalMin = Math.floor(audioDuration / 60)
+    let totalSec = Math.floor(audioDuration % 60)
+    trackDurationMin = totalMin;
+    trackDurationSec = totalSec;
+    show_duration.innerHTML = totalMin < 10 ? `0${totalMin}:${totalSec}` : `${totalMin}:${totalSec}`
+}
+function change_duration(){
 
+    const duration = track.duration;
+    slider_position = duration * (slider.value / 100);
+
+    track.currentTime = slider_position;
+    // let clickedOffsetX = slider_position.offsetX;
+    // let progressWithval = slider.clientWidth;
+    // track.currentTime = (clickedOffsetX / progressWithval) * duration;
+    playsong();
+}
+
+track.addEventListener("timeupdate", () => {
+    let musicCurrentTime = container.querySelector(".currentTime");
+    let audioDuration = track.currentTime;
+    let totalMin = Math.floor(audioDuration / 60)
+    let totalSec = Math.floor(audioDuration % 60)
+    let leftMin = trackDurationMin-totalMin,
+        leftSec = trackDurationSec-totalSec;
+    musicCurrentTime.innerHTML = totalMin < 10 ? `0${totalMin}:${totalSec}` : `${totalMin}:${totalSec}`
+    show_duration.innerHTML = leftMin < 10 ? `0${leftMin}:${leftSec}` : `${leftMin}:${leftSec}`
+})
